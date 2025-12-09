0a44935aefe3dd7193267caa45f86b95f7c22aa8
Ronbu <ronxs2@gmail.com>
2025-12-09 22:17:53 +0200

    movie api CRUD functionality need publisher features

--- routes/moviesRoute.js
+++ routes/moviesRoute.js
@@ -2,10 +2,15 @@ const express = require('express');
 const router = express.Router();
 const moviesController = require('../controllers/moviesController');
 
-router.get('/', moviesController.getAllMovies);
-router.get('/:id', moviesController.getMovieById);
-router.post('/', moviesController.addMovie);
-router.put('/:id', moviesController.updateMovie);
-router.delete('/:id', moviesController.deleteMovie);
+router.get("/", moviesController.getAllMovies);
+
+router.get("/:id", moviesController.getMovieById);
+
+router.post("/", moviesController.createMovie);
+
+router.delete("/:id", moviesController.deleteMovie);
+
+router.put("/:id", moviesController.updateMovie);
 
 module.exports = router;
+
